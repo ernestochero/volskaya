3c9f5851ad0e37f6399349fe4b0a4bfe5dedc67d
Ernesto Chero <informatico240@gmail.com>
2019-04-11 01:48:36 -0500

    add trait OrderType and StatusOrderType

--- src/main/scala/model/OrderModel.scala
+++ src/main/scala/model/OrderModel.scala
@@ -4,14 +4,53 @@ import akka.http.scaladsl.model.DateTime
 import org.bson.types.ObjectId
 
 case class Order(id:ObjectId = new ObjectId(),
-                 orderType: OrderType,
-                 creationTime: String,
-                 statusFromCyclist: StatusFromCyclist,
                  finalPrice:Option[Double],
                  isPaid: Option[Boolean],
-                 paymentDay: Option[String],
-                 paymentMethod: Option[DateTime]
+                 paymentDateTime: Option[DateTime],
+                 paymentMethod: Option[String]
                 )
 //TODO: ask about status order
-case class OrderType(name: String, description: String)
-case class StatusFromCyclist(timeAccepted:Option[String], timeDeclined:Option[String])
+//TODO: make TypeStatusOrder an Enumeration
+case class StatusOrder(name: String,
+                       orderType: String,
+                       description: String,
+                       isCanceled:Option[Boolean],
+                       doAction:String)
+
+sealed trait OrderType {
+  val description:String
+}
+case object DELIVERY extends OrderType {
+  override val description: String = "week, months of the year"
+}
+case object OPERATION extends OrderType {
+  override val description: String = "but depending on your needs it can also be a good approach:"
+}
+
+sealed trait StatusOrderType {
+  val description: String
+}
+
+case object EMITIDO_CLIENTE extends StatusOrderType {
+  override val description: String = "Emitido por el cliente"
+}
+
+case object RECHAZADO_ADMINISTRADO extends StatusOrderType {
+  override val description: String =  "La order fue rechazada por el administrador "
+}
+
+
+
+// val orderTypes:List[_ <: OrderType] = DELIVERY :: OPERATION :: Nil
+
+
+/*
+* // a "heavier"
+package com.acme.app {
+    sealed trait Margin
+    case object TOP extends Margin
+    case object RIGHT extends Margin
+    case object BOTTOM extends Margin
+    case object LEFT extends Margin
+}
+* */
--- src/main/scala/model/test.scala
+++ src/main/scala/model/test.scala
@@ -1,8 +1,22 @@
 package model
-import org.joda.time.DateTime
+import org.joda.time._
+import org.joda.time.format.DateTimeFormat
 
 object test extends App {
-  override def main(args: Array[String]): Unit = {
-    println(s"time is ${DateTime.now()}")
+  override def main(args: Array[String]) = {
+    val pattern = "dd-MM-yyyy hh:mm:ss aa"
+    val beforeSleep = DateTime.now()
+
+    val afterSleep = DateTime.now()
+    println(s"before : ${beforeSleep}")
+    println(s"after : ${afterSleep.getMillis}")
+    val inputTime = afterSleep.toString(pattern)
+    println(s"convert : ${inputTime}")
+    val dt = DateTime.parse(inputTime,DateTimeFormat.forPattern(pattern))
+    println(s"la xx ${dt.getMillis}")
+
+    val orderTypes:List[_ <: OrderType] = DELIVERY :: OPERATION :: Nil
+    val findOrderType = orderTypes.find( c => c.toString == "DELIVERY")
+    println(s"findOrderType ${ findOrderType.fold("Class not found"){_.description}  }")
   }
 }
