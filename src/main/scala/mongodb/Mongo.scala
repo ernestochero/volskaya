d98dc2cbeacdb7e4d322b882489f7c244c551e05
Ernesto Chero <informatico240@gmail.com>
2019-04-25 00:44:36 -0500

    adding codecs for models

--- src/main/scala/mongodb/Mongo.scala
+++ src/main/scala/mongodb/Mongo.scala
@@ -7,10 +7,21 @@ import org.mongodb.scala.bson.codecs.{DEFAULT_CODEC_REGISTRY, Macros}
 import org.mongodb.scala.{MongoClient, MongoCollection, MongoDatabase}
 
 object Mongo {
-  lazy val userCodeProvider = Macros.createCodecProvider[User]()
+  lazy val userCodecProvider = Macros.createCodecProvider[User]()
+  lazy val deviceCodecProvider = Macros.createCodecProvider[Device]()
+  lazy val userCyclistCodecProvider = Macros.createCodecProvider[UserCyclist]()
+  lazy val userProducerCodecProvider = Macros.createCodecProvider[UserProducer]()
+  lazy val orderCodecProvider = Macros.createCodecProvider[Order]()
+  lazy val goalCodecProvider = Macros.createCodecProvider[Goal]()
+  lazy val goalCanceledCodecProvider = Macros.createCodecProvider[GoalCanceled]()
+  lazy val coordinateCodecProvider = Macros.createCodecProvider[Coordinate]()
+  lazy val productCodecProvider = Macros.createCodecProvider[Product]()
   lazy val config = ConfigFactory.load()
   lazy val mongoClient: MongoClient = MongoClient(config.getString("mongo.uri"))
-  lazy val codecRegistry = fromRegistries(fromProviders(userCodeProvider), DEFAULT_CODEC_REGISTRY)
+  lazy val codecRegistry = fromRegistries(
+    fromProviders(userCodecProvider, deviceCodecProvider, userCyclistCodecProvider, userProducerCodecProvider,
+      orderCodecProvider, goalCodecProvider, goalCanceledCodecProvider, coordinateCodecProvider, productCodecProvider),
+    DEFAULT_CODEC_REGISTRY)
   lazy val database: MongoDatabase = mongoClient.getDatabase(config.getString("mongo.database")).withCodecRegistry(codecRegistry)
 
   lazy val usersCollection: MongoCollection[User] = database.getCollection[User]("users")
