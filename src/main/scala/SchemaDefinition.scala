33526975a7496bd9d41997ed02679e90e68e3c95
Ernesto Chero <informatico240@gmail.com>
2019-05-04 11:23:27 -0500

    refactoring the schema definition

--- src/main/scala/SchemaDefinition.scala
+++ src/main/scala/SchemaDefinition.scala
@@ -3,160 +3,46 @@ import play.api.libs.json.Json
 import repository.UserRepo
 import sangria.schema._
 import sangria.marshalling.playJson._
-
+import sangria.macros.derive.{InputObjectTypeName, deriveInputObjectType, deriveObjectType}
 object SchemaDefinition {
 
-  implicit val ProductType = ObjectType("Product", "product description",
-    fields[Unit, Product](
-      Field("name", StringType, resolve = _.value.name),
-      Field("description", StringType, resolve = _.value.description),
-      Field("photo", OptionType(StringType), resolve = _.value.photo),
-      Field("isEspecial", OptionType(BooleanType), resolve = _.value.isSpecial)
-    ))
+  /* type from classes */
 
+  implicit val ProductType = deriveObjectType[Unit, Product]()
   implicit val productFormat = Json.format[Product]
+  implicit val ProductInputType = deriveInputObjectType[Product](InputObjectTypeName("ProductFieldsInput"))
 
-  implicit val ProductInputType = InputObjectType[Product]("ProductFieldsInput", List(
-    InputField("name", StringType),
-    InputField("description", StringType),
-    InputField("photo", StringType),
-    InputField("isEspecial", OptionInputType(StringType))
-  ))
-
-  implicit val CoordinateType = ObjectType("Coordinate", "coordinate description",
-    fields[Unit, Coordinate](
-      Field("latitude", FloatType, resolve = _.value.latitude),
-      Field("longitude", FloatType, resolve = _.value.longitude)
-    ))
-
+  implicit val CoordinateType = deriveObjectType[Unit, Coordinate]()
   implicit val coordinateFormat = Json.format[Coordinate]
+  implicit val CoordinateInputType = deriveInputObjectType[Coordinate](InputObjectTypeName("CoordinateFieldsInput"))
 
-  implicit val CoordinateInputType = InputObjectType[Coordinate]("CoordinateFieldsInput", List(
-    InputField("latitude", FloatType),
-    InputField("longitude", FloatType)
-  ))
-
-  implicit val GoalCanceledType = ObjectType("GoalCanceled", "goalCanceled description",
-    fields[Unit, GoalCanceled](
-      Field("time", OptionType(StringType), resolve = _.value.time),
-      Field("reason", OptionType(StringType), resolve = _.value.reason)
-    ))
-
+  implicit val GoalCanceledType = deriveObjectType[Unit, GoalCanceled]()
   implicit val goalCanceledFormat = Json.format[GoalCanceled]
+  implicit val GoalCanceledInputType = deriveInputObjectType[GoalCanceled](InputObjectTypeName("GoalCanceledFieldsInput"))
 
-  implicit val GoalCanceledInputType = InputObjectType[GoalCanceled]("GoalCanceledFieldsInput", List(
-    InputField("time", OptionInputType(StringType)),
-    InputField("reason", OptionInputType(StringType))
-  ))
-
-  implicit val GoalType = ObjectType("Goal", "goal description",
-    fields[Unit, Goal](
-      Field("products", OptionType(ListType(ProductType)), resolve = _.value.products),
-      Field("userCyclistId", OptionType(StringType), resolve = _.value.userCyclistId),
-      Field("goalCoordinate", OptionType(CoordinateType), resolve = _.value.goalCoordinate),
-      Field("goalTypeName", OptionType(StringType), resolve = _.value.goalTypeName),
-      Field("goalCanceled", OptionType(GoalCanceledType), resolve = _.value.goalCanceled)
-    ))
-
+  implicit val GoalType = deriveObjectType[Unit, Goal]()
   implicit val goalFormat = Json.format[Goal]
+  implicit val GoalInputType = deriveInputObjectType[Goal](InputObjectTypeName("GoalFieldsInput"))
 
-  implicit val GoalInputType = InputObjectType[Goal]("GoalFieldsInput", List(
-    InputField("products", OptionInputType(ListInputType(ProductInputType))),
-    InputField("userCyclistId", OptionInputType(StringType)),
-    InputField("goalTypeName", OptionInputType(CoordinateInputType)),
-    InputField("goalCoordinate", OptionInputType(StringType)),
-    InputField("goalCanceled", OptionInputType(GoalCanceledInputType))
-  ))
-
-  implicit val OrderType = ObjectType("Order", "order description",
-    fields[Unit, Order](
-      Field("orderTypeName", OptionType(StringType), resolve = _.value.orderTypeName),
-      Field("statusOrderTypeName", OptionType(StringType), resolve = _.value.statusOrderTypeName),
-      Field("kilometers", OptionType(FloatType), resolve = _.value.kilometers),
-      Field("finalPrice", OptionType(FloatType), resolve = _.value.finalPrice),
-      Field("isPaid", OptionType(BooleanType), resolve = _.value.isPaid),
-      Field("paymentDateTime", OptionType(StringType), resolve = _.value.paymentDateTime),
-      Field("paymentMethod", OptionType(StringType), resolve = _.value.paymentMethod),
-      Field("goals",OptionType(ListType(GoalType)), resolve = _.value.goals)
-    ))
-
+  implicit val OrderType = deriveObjectType[Unit, Order]()
   implicit val orderFormat = Json.format[Order]
+  implicit val OrderInputType = deriveInputObjectType[Order](InputObjectTypeName("OrderFieldsInput"))
 
-  implicit val OrderInputType = InputObjectType[Order]("OrderFieldsInput", List(
-    InputField("orderTypeName", OptionInputType(StringType)),
-    InputField("statusOrderTypeName", OptionInputType(StringType)),
-    InputField("kilometers", OptionInputType(FloatType)),
-    InputField("finalPrice", OptionInputType(FloatType)),
-    InputField("isPaid", OptionInputType(BooleanType)),
-    InputField("paymentDateTime", OptionInputType(StringType)),
-    InputField("paymentMethod", OptionInputType(StringType)),
-    InputField("goals", OptionInputType(ListInputType(GoalInputType)))
-  ))
-
-
-   /*
-   *  Device
-   * */
-
-  implicit val DeviceType = ObjectType("Device", "device description",
-    fields[Unit, Device](
-      Field("name", StringType, resolve = _.value.name),
-      Field("number", StringType, resolve = _.value.number),
-      Field("imei", StringType, resolve = _.value.imei),
-      Field("token", OptionType(StringType), resolve = _.value.token)
-    ))
-
-  /*implicit val DeviceType = deriveObjectType[Unit, Device]()*/
-
+  implicit val DeviceType = deriveObjectType[Unit, Device]()
   implicit val deviceFormat  = Json.format[Device]
+  implicit val DeviceInputType = deriveInputObjectType[Device](InputObjectTypeName("DeviceFieldsInput"))
 
-/*  implicit val DeviceInputType = deriveInputObjectType[Device](
-    InputObjectTypeName("DeviceFieldsInput"))*/
-
-  implicit val DeviceInputType = InputObjectType[Device]("DeviceFieldsInput", List(
-    InputField("name", StringType),
-    InputField("number", StringType),
-    InputField("imei", StringType),
-    InputField("token", OptionInputType(StringType))
-  ))
-
-
-  /*
-  * User
-  **/
-
-  implicit val UserProducerType = ObjectType("UserProducer", "userProducer description",
-    fields[Unit, UserProducer](
-      Field("nameCompany", StringType, resolve = _.value.nameCompany),
-      Field("address", StringType, resolve = _.value.address),
-      Field("phone", StringType, resolve = _.value.phone),
-      Field("ruc", StringType, resolve = _.value.ruc)
-    ))
-
+  implicit val UserProducerType = deriveObjectType[Unit, UserProducer]()
   implicit val userProducerFormat = Json.format[UserProducer]
+  implicit val UserProducerInputType = deriveInputObjectType[UserProducer](InputObjectTypeName("DeviceFieldsInput"))
 
-  implicit val UserProducerInputType = InputObjectType[UserProducer]("UserProducerFieldsInput", List(
-    InputField("nameCompany", StringType),
-    InputField("address", StringType),
-    InputField("phone", StringType),
-    InputField("ruc", StringType)
-  ))
-
-
-  implicit val UserCyclistType = ObjectType("UserCyclist", "userCyclist description",
-    fields[Unit, UserCyclist](
-      Field("firstName", StringType, resolve = _.value.firstName ),
-      Field("lastName", StringType, resolve = _.value.lastName),
-      Field("dni", StringType, resolve = _.value.dni)
-    ))
-
+  implicit val UserCyclistType = deriveObjectType[Unit, UserCyclist]()
   implicit val userCyclistFormat  = Json.format[UserCyclist]
+  implicit val UserCyclistInputType = deriveInputObjectType[UserCyclist](InputObjectTypeName("DeviceFieldsInput"))
 
-  implicit val UserCyclistInputType = InputObjectType[UserCyclist]("UserCyclistFieldsInput", List(
-    InputField("firstName", StringType),
-    InputField("lastName", StringType),
-    InputField("dni", StringType)
-  ))
+  implicit val UserType = deriveObjectType[Unit, UserDomain]()
+  implicit val userFormat = Json.format[UserDomain]
+  implicit val UserInputType = deriveInputObjectType[UserDomain](InputObjectTypeName("UserFieldsInput"))
 
   implicit val PersonType: InterfaceType[Unit, Person] = InterfaceType("Person", "person description",
     () => fields[Unit, Person](
@@ -165,18 +51,10 @@ object SchemaDefinition {
       Field("dni", StringType, resolve = _.value.dni)
     ))
 
-  implicit val UserType = ObjectType("User", "user description",
-    fields[Unit, UserDomain](
-      Field("id", OptionType(StringType), resolve = _.value.id),
-      Field("device", OptionType(DeviceType), resolve = _.value.device),
-      Field("userCyclist", OptionType(UserCyclistType), resolve = _.value.userCyclist),
-      Field("userProducer", OptionType(UserProducerType), resolve = _.value.userProducer),
-      Field("email", OptionType(StringType), resolve = _.value.email),
-      Field("password", OptionType(StringType), resolve = _.value.password),
-      Field("isAuthenticated", OptionType(BooleanType), resolve = _.value.isAuthenticated),
-      Field("orders", OptionType(ListType(OrderType)), resolve = _.value.orders)
-    ))
+  /* custom types*/
 
+
+  /* Arguments*/
   val IdArg = Argument("id", OptionInputType(StringType))
   val DeviceArg = Argument("device", OptionInputType(DeviceInputType))
   val UserCyclistArg = Argument("userCyclist", OptionInputType(UserCyclistInputType))
